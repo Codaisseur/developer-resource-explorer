abf06e960163ea96cbb4ba30b625664c62dab1da
Kelley van Evert <hello@kelleyvanevert.nl>
2020-03-08 02:43:30 +0100

    5. Another computed parametrized selector

--- src/App.js
+++ src/App.js
@@ -14,14 +14,35 @@ const selectResources = state => {
   return state.resources;
 };
 
+const selectDevelopers = state => {
+  return state.developers;
+};
+
+const selectDevelopersFavoritesResources = developerId => state => {
+  const developer = state.developers.find(dev => dev.id === developerId);
+  if (!developer) {
+    return [];
+  }
+
+  return state.resources.filter(resource => {
+    return developer.favorites.includes(resource.id);
+  });
+};
+
 function App() {
   const statistics = useSelector(selectStatistics);
   const resources = useSelector(selectResources);
+  const developers = useSelector(selectDevelopers);
 
+  const [developerId, setDeveloperId] = useState(2);
   const [favoriteId, setFavoriteId] = useState(2);
 
   const developersWithThisFavorite = useSelector(
     selectDevelopersWithFavorite(favoriteId)
+  );
+
+  const favoriteResources = useSelector(
+    selectDevelopersFavoritesResources(developerId)
   );
 
   return (
@@ -37,27 +58,56 @@ function App() {
           <p>resources</p>
         </div>
       </div>
-      <h2>
-        Who likes{" "}
-        <select
-          value={favoriteId}
-          onChange={e => setFavoriteId(parseInt(e.target.value))}
-        >
-          {resources.map(resource => {
-            return (
-              <option key={resource.id} value={resource.id}>
-                {resource.name}
-              </option>
-            );
-          })}
-        </select>
-        ?
-      </h2>
-      <ul>
-        {developersWithThisFavorite.map(dev => {
-          return <li key={dev.id}>{dev.name}</li>;
-        })}
-      </ul>
+
+      <div style={{ display: "flex" }}>
+        <div style={{ marginRight: "2rem" }}>
+          <h2>
+            Who likes{" "}
+            <select
+              value={favoriteId}
+              onChange={e => setFavoriteId(parseInt(e.target.value))}
+            >
+              {resources.map(resource => {
+                return (
+                  <option key={resource.id} value={resource.id}>
+                    {resource.name}
+                  </option>
+                );
+              })}
+            </select>
+            ?
+          </h2>
+          <ul>
+            {developersWithThisFavorite.map(dev => {
+              return <li key={dev.id}>{dev.name}</li>;
+            })}
+          </ul>
+        </div>
+
+        <div>
+          <h2>
+            What are{" "}
+            <select
+              value={developerId}
+              onChange={e => setDeveloperId(parseInt(e.target.value))}
+            >
+              {developers.map(dev => {
+                return (
+                  <option key={dev.id} value={dev.id}>
+                    {dev.name}
+                  </option>
+                );
+              })}
+            </select>
+            's favorites?
+          </h2>
+          <ul>
+            {favoriteResources.map(resource => {
+              return <li key={resource.id}>{resource.name}</li>;
+            })}
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
